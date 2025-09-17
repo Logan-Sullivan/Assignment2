10fc113b63c2a06be9b13184495eaa1fdbbaa4e5
Logan-Sullivan <lsullivan4@missouriwestern.edu>
2025-09-17 15:03:18 -0500

    Comment Goals of program and req functions

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,37 @@
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
-    }
+
 }
+/* Driver Program
+*
+* Load records from input.txt loading each record into a student object
+* Calc the %score and grade for that Student Object Then place it into Academic_Class Arraylist
+*
+* list the records from input including %score and grades
+*
+* Delete students 42P4 and 45A3
+*
+* list the records again after they are dropped
+*
+* add new students to arraylist
+*
+* calc grades and sort from lower to higher %score
+*
+* print ArrayList
+*
+*
+* */
+
+
+
+/* Req Functions:
+* void AddStudent(Arraylist, Academic_Class, Student Obj)
+* adds students to/from arraylist
+*
+* void DeleteStudent(Arraylist Academic_Class, String StudentID)
+* Deletes student from arraylist with StudentID
+*
+* void SortLarge(Arraylist Academic_Class)
+* bubble sort arraylist larger to smaller based on %score
+*
+*
+* */
